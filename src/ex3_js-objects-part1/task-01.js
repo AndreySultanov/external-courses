47bb9fc5533347771133386a897a8a321682ff9e
DESKTOP-33MRRGO\79206 <skar4500.1d@gmail.com>
2021-03-11 22:36:33 +0300

    complete ex_3 task-01.js

---
+++ src/ex3_js-objects-part1/task-01.js
@@ -0,0 +1,9 @@
+const product = {};
+
+product.temperature = 4;
+product.manufactureDate = '01.01.2000';
+product.packagingDate = '01.02.2000';
+product.expirationDate = '01.03.2000';
+product.quality = true;
+
+delete product.temperature;
